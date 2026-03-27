c91a19b77e6b9223ff5556c438df814365a7715b
snowdutra <gusdutratelles469@gmail.com>
2026-03-27 20:41:01 -0300

    adiciona implementação do pipeline ETL com extração, transformação e carga de dados; inclui configuração de ambiente, Docker e documentação; cria estrutura de diretórios e arquivos necessários

--- etl_python/src/config.py
+++ etl_python/src/config.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 class Settings:
     api_base_url: str = os.getenv("API_BASE_URL", "https://api.openbrewerydb.org/v1/breweries")
     db_host: str = os.getenv("DB_HOST", "localhost")
-    db_port: int = int(os.getenv("DB_PORT", 5432))
+    db_port: int = int(os.getenv("DB_PORT", 5433))
     db_name: str = os.getenv("DB_NAME", "etl_db")
     db_user: str = os.getenv("DB_USER", "etl_user")
     db_password: str = os.getenv("DB_PASSWORD", "etl_pass")
